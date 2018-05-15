51d881b4156134f8efae3365d976b7523c030c5f
Donna Cerrio <denique86@yahoo.com>
2018-05-15 01:33:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(name){
 }
 //
 function happyHolidayTo(happyHolidayTo, name){
-return ("${happyHolidayTo}, ${name}")
+return `Happy ${holiday}, ${name}`)
 // }
 //
 function holidayCountdown(holiday, days){
